4f511e602563bf55fd5113b74530fe93d025d383
yuri315 <hi_smiley89@yahoo.co.jp>
2022-12-20 17:23:48 +0900

    アセットファイル等はrails generate時に自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module TenkiHairset
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.generators do |g| # ここから追記
+      g.assets false          # CSS, JavaScriptファイルを自動生成しない
+      g.helper     false      # helperファイルを自動生成しない
+    end  # ここまで追記
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
