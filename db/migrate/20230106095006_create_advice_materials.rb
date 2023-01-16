4896875e303ddaadc38afecfb5e83906a5212059
yuri315 <hi_smiley89@yahoo.co.jp>
2023-01-16 14:03:51 +0900

    my髪質、天気controllerの実装

--- db/migrate/20230106095006_create_advice_materials.rb
+++ db/migrate/20230106095006_create_advice_materials.rb
@@ -2,7 +2,7 @@ class CreateAdviceMaterials < ActiveRecord::Migration[6.1]
   def change
     create_table :advice_materials do |t|
       t.string :city, null: false
-      t.integer :forecast_date, null: false
+      t.date :forecast_date, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
