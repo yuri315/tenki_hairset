63ff2dfef0629b9f1de57e01209d2f0b03fba7f6
yuri315 <hi_smiley89@yahoo.co.jp>
2023-03-13 18:20:50 +0900

    API情報の取得

--- db/migrate/20230106023716_create_cities.rb
+++ db/migrate/20230106023716_create_cities.rb
@@ -2,7 +2,8 @@ class CreateCities < ActiveRecord::Migration[6.1]
   def change
     create_table :cities do |t|
       t.string :city_name, null: false,  comment: "都市名"
-      t.integer "location_id", null: false, comment: "locationのid"
+      t.float "lat", null: false, comment: "locationの緯度"
+      t.float "lon", null: false, comment: "locationの経度"
 
       t.timestamps
     end
---
+++ lib/api/open_weather_map/request.rb
@@ -0,0 +1,35 @@
+module Api
+  module OpenWeatherMap
+    class Request
+      attr_accessor :data
+
+
+      def initialize(lat,lon)
+        @data = {
+          lat: lat,
+          lon: lon,
+          units: 'metric',
+          appid: ENV['WEATHER_API_KEY'],
+        }
+      end
+
+      def request
+        client = HTTPClient.new
+        request = client.get(ENV['WEATHER_URL'], data)
+        JSON.parse(request.body)
+      end
+  
+      def self.attributes_for(attrs)
+        {
+          temp_max: attrs['temp']['max'],
+          temp_min: attrs['temp']['min'],
+          humidity: attrs['humidity'],
+          #temp_feel: attrs['main']['feels_like'],
+          weather_id: attrs['weather'][0]['id'],
+          weather_icon: attrs['weather'][0]['icon'],
+          wind_speed: attrs['wind_speed']
+      }
+      end
+    end
+  end
+end
